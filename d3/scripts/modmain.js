9ba616579522964a77841fc2376d17a38be5ce35
David Campbell <d.cam09@gmail.com>
2016-03-28 23:46:09 -0400

    Update

--- d3/scripts/modmain.js
+++ d3/scripts/modmain.js
@@ -20,7 +20,7 @@ function fisheyemove () {
       node.each(function(d) { d.fisheye = fisheye(d); })
         .attr("cx", function(d) { return d.fisheye.x; })
         .attr("cy", function(d) { return d.fisheye.y; })
-        .attr("r", function(d) { return d.fisheye.z * d.count-4; });
+        .attr("r", function(d) { return d.fisheye.z * (3+Math.log(d.helpful+1)); });
 
       link.attr("x1", function(d) { return d.source.fisheye.x; })
         .attr("y1", function(d) { return d.source.fisheye.y; })
@@ -28,13 +28,15 @@ function fisheyemove () {
         .attr("y2", function(d) { return d.target.fisheye.y; });
   }
   
-    
+  
 var width = 960,
     height = 800;
 
-var nodeLocation= null;
-var linkDependencies = null;
+var nodeLocation= {};
+var linkDependencies = [];
 var default_stroke_opacity = 0.1;
+var highlighted = null; 
+
 var force = d3.layout.force()
     .size([width, height])
     .charge(-70)
@@ -59,9 +61,18 @@ var svg = d3.select("body").append("svg")
 var link = svg.selectAll(".link"),
     node = svg.selectAll(".node");
 
-d3.json("100000graph.json", function(error, graph) {
+function main(filename){
+d3.json(filename, function(error, graph) {
   if (error) throw error;
-  var nodeLocation = {};
+  //var nodeLocation = {};
+  
+  var n = graph.nodes.length;
+  for (var i=0; i < n; i++) {
+    linkDependencies[i] = [];
+    for (var j=0; j < n; j++) {
+      linkDependencies[i][j] = false;
+    }
+  }
   var temp = 0;
   graph.nodes.forEach(function(n){
     n["x"] = randint2(width);
@@ -73,7 +84,11 @@ d3.json("100000graph.json", function(error, graph) {
   graph.links.forEach(function(l) {
     l.source = nodeLocation[l.source];
     l.target = nodeLocation[l.target];
+    linkDependencies[l.source][l.target] = true;
+    linkDependencies[l.target][l.source] = true;
   });
+
+
 
   force
       .nodes(graph.nodes)
@@ -90,15 +105,61 @@ d3.json("100000graph.json", function(error, graph) {
   node = node.data(graph.nodes)
     .enter().append("circle")
       .attr("class", "node")
-      .attr("r", function(d){return 3 + Math.log(d.helpful);})
+      .attr("r", function(d){return 3 + 1.5*Math.log(d.helpful+1);})
       .style("fill", function(d){
         var avg_score = d.scores / d.count;
         return review_color(avg_score);
+      })
+      .on('mouseover', function(d) { // Highlights the node underneath the mouse.
+      if (graph.mouseoutTimeout) {
+        clearTimeout(graph.mouseoutTimeout);
+        graph.mouseoutTimeout = null;
+      }
+      highlightObject(d);
+        })
+        .on('mouseout', function() { // Removes the highlight of the node.
+      if (graph.mouseoutTimeout) {
+        clearTimeout(graph.mouseoutTimeout);
+        graph.mouseoutTimeout = null;
+        console.log('bye');
+      }
+      graph.mouseoutTimeout = setTimeout(function() {
+        highlightObject(null);
+        }, 300);
       })
       .on("dblclick", dblclick)
       .call(drag);
       svg.on('mousemove', null); 
 });
+}
+
+function highlightObject(obj) {
+    if (obj) {
+      if (obj !== highlighted) {
+        node.attr('fill-opacity', function(d) {
+            // If the node is not the mouseover-ed node or link-dependent on that node.
+          if (obj !== d
+                  && !linkDependencies[nodeLocation[obj.name]][nodeLocation[d.name]]
+                && !linkDependencies[nodeLocation[d.name]][nodeLocation[obj.name]]) {
+            return (.1);
+          }
+          return 1; // If it is that node or connected to that node.
+        });
+        link.attr('stroke-opacity', function(d) {
+          if (obj !== d.source && obj !== d.target) return (.1);
+          return 1; 
+        });
+      }
+      highlighted = obj;
+    } else {
+        // Remove any highlighted nodes.
+      if (highlighted) {
+        node.attr('fill-opacity', 1);
+        link.attr('stroke-opacity', default_stroke_opacity);
+      }
+      highlighted = null;
+    }
+  }
 
 function tick() {
   link.attr("x1", function(d) { return d.source.x; })
@@ -116,4 +177,18 @@ function dblclick(d) {
 
 function dragstart(d) {
   d3.select(this).classed("fixed", d.fixed = true);
+}
+
+function start() {
+  var selection1 = document.getElementById("select_file");
+    var str = selection1.options[selection1.selectedIndex].text;
+  if (str == '10000graph') {
+        main('networks/10000graph.json');
+  } else if (str == '100000graph') {
+    main('networks/100000graph.json');
+  } else if (str == '100000graphb') {
+    main('networks/100000graphb.json');
+  } else if (str == 'full graph') {
+    main('networks/agraph1000.json');
+  }
 }
